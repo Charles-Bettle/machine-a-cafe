b5ce12d44588fdd0636e0a4ff42341ffdaef2216
Charles Bettle <charles.bettle@free.fr>
2017-06-18 11:01:30 +0200

    machine a café
    1er envoi de tous les éléments

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//alert('TEST');
+
+
+(function(){
+
+
+     let cibleB=document.querySelector('p');
+     let choixCafe={
+        choix1:'CAFE LONG',
+        choix2:"ESPRESSO",
+        choix3:'CAPPUCCINO',
+        choix4:'NOISETTE',
+    };
+
+    cibleB.textContent=choixCafe.choix1;
+
+    
+    // cibleB.textContent=choixCafe.choix2;
+    // cibleB.textContent=choixCafe.choix3;
+    // cibleB.textContent=choixCafe.choix4;
+
+})();
